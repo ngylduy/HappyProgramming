4387d409e4eaf3cafee11cfdccb7ddefdbdb3fc0
phucne123111 <134253765+phucne123111@users.noreply.github.com>
2023-07-08 15:46:35 +0700

    fix lỗi
    lỗi icon khi window.location.reload trên reactjs

--- Fontend/src/index.js
+++ Fontend/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import '../node_modules/startbootstrap-sb-admin-2/vendor/jquery/jquery.min.js'
+import '../node_modules/startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min.js'
+import '../node_modules/startbootstrap-sb-admin-2/vendor/fontawesome-free/css/all.min.css'
 import '../node_modules/startbootstrap-sb-admin-2/css/sb-admin-2.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer } from 'react-toastify';
