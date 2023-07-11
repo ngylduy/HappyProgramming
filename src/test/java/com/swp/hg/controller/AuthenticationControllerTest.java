9eb739d510e48d5326f238a747ea89cd00e5ca5d
ngylduy <duynlhe161345@fpt.edu.vn>
2023-07-11 20:22:46 +0700

    Updete

---
+++ src/test/java/com/swp/hg/controller/AuthenticationControllerTest.java
@@ -0,0 +1,26 @@
+package com.swp.hg.controller;
+
+import org.junit.jupiter.api.Test;
+
+class AuthenticationControllerTest {
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void resetPassword() {
+    }
+
+    @Test
+    void confirm() {
+    }
+
+    @Test
+    void saveNewPassword() {
+    }
+
+    @Test
+    void changePassword() {
+    }
+}
